61d76e962cd44c0d54d9a50cf21f0e1ee35f8a43
Angel04Jesus19 <angeldejesusmendozadieguez130@gmail.com>
2025-12-05 01:36:40 -0600

    Agregar rutas de home y dependencias

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DependenciaController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-| Se accede a ellas con el prefijo /api
-*/
-
 Route::get('/dependencias', [DependenciaController::class, 'index']);
 Route::get('/dependencias/{id}', [DependenciaController::class, 'show']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test-db', function () {
-    try {
-        $result = DB::select('SELECT TOP 1 * FROM Dependencias'); // ajusta el nombre de la tabla si hace falta
-        return response()->json([
-            'ok' => true,
-            'data' => $result,
-        ]);
-    } catch (\Exception $e) {
-        return response()->json([
-            'ok' => false,
-            'error' => $e->getMessage(),
-        ], 500);
-    }
+Route::get('/', function () {
+    return response()->json([
+        'status'  => 'ok',
+        'message' => 'SIGSS API funcionando en Azure'
+    ]);
 });
